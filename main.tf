2f9b40ad5d1ad8753e7fd3c14de0d5fbd7cff94b
Ruizhe1996 <81406105+Ruizhe1996@users.noreply.github.com>
2024-07-25 12:13:57 +0800

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,31 +1,30 @@
 provider "aws" {
-  region = "ap-southeast-1"
+  region = "us-east-1"
 }
 
-#Creating my VPC
+# 1. Create VPC
 
-resource "aws_vpc" "main" {
-    cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "prod-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "Production"
 
-    tags = {
-        Name = "VPC-Lab"
-    }
+  }
 }
 
-#Creating Internet gateway for the access point
+# 2. Create Internet Gateway
 
 resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
-
+  vpc_id = aws_vpc.prod-vpc.id
   tags = {
     Name = "main"
   }
 }
 
-#RouteTable configuration for my public subnet to have a path to internet, another route table for my nat gateway
+# 3. Create Custom Route Table
 
-resource "aws_route_table" "Public-routetable" {
-  vpc_id = aws_vpc.main.id
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.prod-vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -38,126 +37,115 @@ resource "aws_route_table" "Public-routetable" {
   }
 
   tags = {
-    Name = "Main-routetable"
+    Name = "Prod"
   }
 }
 
-#Nat-gateway for private subnet to access internet 
+# 4. Create a Subnet
 
-resource "aws_route_table" "Natgateway-route" {
-  vpc_id = aws_vpc.main.id
+resource "aws_subnet" "subnet-1" {
+  vpc_id     = aws_vpc.prod-vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.Natgateway-a.id
+  tags = {
+    Name = "prod-subnet"
+  }
+}
+
+# 5. Associate Subnet with Route Table - the path of the connection - while this seems redundant, one VPC can have multiple route table 
+
+ resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.prod-route-table.id
+}
+
+# 6. Create Security Group to allow 22, 80, 443 - What kind of port is allowed to make connection
+
+resource "aws_security_group" "allow_webtraffic" {
+  name        = "allow_webtraffic"
+  description = "Allow web inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.prod-vpc.id
+
+  ingress {
+    description = "HTTPS"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTP"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "AllowAll"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
   }
 
   tags = {
-    Name = "Private-natgateway-routetable"
+    Name = "allow_webtraffic"
   }
 }
 
-#For subnet-a to be able to connect to S3 without going through internet
+# 7. Create a network interface with an ip in the subnet that was created in 4. - because the instance do not have a entry and exit point 
 
-resource "aws_route_table" "s3-private-connection" {
-    vpc_id = aws_vpc.main.id
+resource "aws_network_interface" "webserver-nic" {
+  subnet_id       = aws_subnet.subnet-1.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_webtraffic.id]
 
-    route {
-        cidr_block = "0.0.0.0/0"
-        vpc_endpoint_id = "aws_vpc_endpoint.s3"
-    }
 }
 
-#Creating 4 Subnets, 2 Private Subnets and 2 Public Subnets
 
-resource "aws_subnet" "public-a" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.10.0/24"
-  availability_zone = "ap-southeast-1a"
-
-  tags = {
-    Name = "public-subnet-a"
-  }
-}
-
-resource "aws_subnet" "public-c" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.20.0/24"
-  availability_zone = "ap-southeast-1c"
-
-  tags = {
-    Name = "public-subnet-c"
-  }
-}
-
-resource "aws_subnet" "private-a" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.100.0/24"
-  availability_zone = "ap-southeast-1a"
-
-  tags = {
-    Name = "private-subnet-a"
-  }
-}
-
-resource "aws_subnet" "private-c" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.200.0/24"
-  availability_zone = "ap-southeast-1c"
-
-  tags = {
-    Name = "private-subnet-c"
-  }
-}
-
-#Routetable association 
-
-resource "aws_route_table_association" "Public-a" {
-  subnet_id       = aws_subnet.public-a.id
-  route_table_id = aws_route_table.Public-routetable.id
-}
-
-resource "aws_route_table_association" "Public-c" {
-  subnet_id       = aws_subnet.public-c.id
-  route_table_id = aws_route_table.Public-routetable.id
-}
-
-resource "aws_route_table_association" "private-natgateway" {
-  subnet_id       = aws_subnet.private-a.id
-  route_table_id = aws_route_table.Natgateway-route.id
-}
-
-resource "aws_route_table_association" "private-s3" {
-  subnet_id       = aws_subnet.private-a.id
-  route_table_id = aws_route_table.s3-private-connection.id
-}
-
-#Creation of EIP for my natgateway
+# 8. Assign an elastic IP address to network interface in step 7
 
 resource "aws_eip" "one" {
   domain                    = "vpc"
-}
- 
-
-#Creating Nat Gateway 
-
-resource "aws_nat_gateway" "Natgateway-a" {
-  allocation_id = aws_eip.one.id
-  subnet_id     = aws_subnet.public-a.id
-
-  tags = {
-    Name = "gw NAT"
-  }
-  depends_on = [aws_internet_gateway.gw]
+  network_interface         = aws_network_interface.webserver-nic.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on = [ aws_internet_gateway.gw ]
 }
 
+# 9. Create Ubuntu server and install apache2 (which is mainly to host webserver)  
 
-#Creating a VPC endpoint so that my S3 can communicate with my private subnet within AWS without going to the internet
+resource "aws_instance" "web-server"{
+    ami = "ami-04a81a99f5ec58529"
+    instance_type = "t2.micro"
+    availability_zone = "us-east-1a"
+    key_name = "main-key"
 
-resource "aws_vpc_endpoint" "s3" {
-    vpc_id = aws_vpc.main.id
-    service_name = "com.amazonaws.ap-southeast-1.s3"
-    #Service name usually com.amazonaws.{region}.{service_name}
-    vpc_endpoint_type = "Interface"
+    network_interface {
+      device_index = 0 
+      network_interface_id = aws_network_interface.webserver-nic.id
+    }
+
+    user_data = <<-EOF
+                #!/bin/bash
+                sudo apt update -y
+                sudo apt install apache2 -y
+                sudo systemctl start apache2
+                sudo bash -c 'echo your very first web server > /var/www/html/index.html'
+                EOF
+
+    tags = {
+        Name = "web-server"
+    }
+
 }
-
